631a425cc1b75e8f2f756964cb5684efd150da90
Mohan Sai Singu <mohansaisingu23@gmail.com>
2024-07-23 15:58:50 -0400

    added code to manage s3 bucket

---
+++ src/main/java/S3FileManager.java
@@ -0,0 +1,63 @@
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.*;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.nio.file.Paths;
+
+public class S3FileManager {
+
+    private final String bucketName;
+    private final S3Client s3Client;
+
+    public S3FileManager(String bucketName) {
+        this.bucketName = bucketName;
+        this.s3Client = S3Client.builder().region(Region.US_EAST_2).build();
+    }
+
+    public void uploadFile(String fileName, String filePath) {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+        s3Client.putObject(request, RequestBody.fromFile(Paths.get(filePath)));
+        System.out.println("File uploaded successfully");
+    }
+
+    public void downloadFile(String fileName, String destinationPath) {
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+        s3Client.getObject(request, Paths.get(destinationPath));
+        System.out.println("File downloaded successfully");
+    }
+
+    public void listFiles() {
+        ListObjectsV2Request request = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .build();
+        ListObjectsV2Response response = s3Client.listObjectsV2(request);
+        response.contents().forEach(content -> System.out.println(content.key() + " (Size: " + content.size() + " bytes)"));
+    }
+
+    public void deleteFile(String fileName) {
+        DeleteObjectRequest delete = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+        s3Client.deleteObject(delete);
+        System.out.println("File deleted successfully");
+    }
+
+    public static void main(String[] args) {
+        String bucketName = System.getenv("BUCKET_NAME");
+        S3FileManager s3 = new S3FileManager(bucketName);
+        System.out.println(bucketName);
+        s3.uploadFile("sample.txt", "/Users/mohansai/Documents/aws/java/sample.txt");
+        s3.listFiles();
+        s3.downloadFile("sample.txt", "/Users/mohansai/Documents/aws/java/output_sample.txt");
+        //s3.deleteFile("sample.txt");
+    }
+}
--- src/main/java/com/myorg/S3FileManagerStack.java
+++ src/main/java/com/myorg/S3FileManagerStack.java
@@ -1,10 +1,11 @@
 package com.myorg;
 
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.CfnOutputProps;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.s3.*;
 
 public class S3FileManagerStack extends Stack {
     public S3FileManagerStack(final Construct scope, final String id) {
@@ -16,9 +17,18 @@ public class S3FileManagerStack extends Stack {
 
         // The code that defines your stack goes here
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "S3FileManagerQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Bucket bucket = Bucket.Builder.create(this, "S3FileManagerBucket")
+                .bucketName("s3-file-manager-bucket")
+                .versioned(false)
+                .encryption(BucketEncryption.S3_MANAGED)
+                .enforceSsl(true)
+                .build();
+
+        new CfnOutput(this, "BucketName", CfnOutputProps.builder()
+                .description("The name of the S3 bucket")
+                .value(bucket.getBucketName())
+                .build());
+
+
     }
 }
